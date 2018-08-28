73623bac4e7d78d977b03be33a59198493d68e41
Bernard Kitchens <bernard.kitchens@gmail.com>
2018-08-28 00:08:54 -0700

    added server and html our server has basic functionality

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require(`express`);
+var http = require(`http`);
+
+
+var app = express();
+
+// sets the app up to handle http requests
+var server = http.Server(app);
+
+// sets up the socket to use our server to handle events
+var io = require('socket.io')(server);
+
+let port = 3000
+server.listen(3000, () => {
+  console.log(`server is listening on port ${port}`);
+});
+
+app.get(`/`, (req, res) => {
+  res.sendFile(__dirname + `/index.html`);
+})
+
+io.on(`connection`, () => {
+  console.log(`A user has connected...`);
+})
